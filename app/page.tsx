4d66d057ad9954928cd8081cefc4088a35f2b3e1
shubhanshu-02 <shubhanshu004@gmail.com>
2025-04-16 00:41:59 +0530

    IGNORED IMPORT

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { FileExplorer } from "@/components/file-explorer";
 import { Folder } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function DrivePage() {
